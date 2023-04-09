f4603e57c138b97c8998881f2fa51f2078ca9399
JOAO DIVINO DA SILVA <60980853+joaodivinod@users.noreply.github.com>
2023-04-08 22:00:08 -0300

    Sessão de tecnologias e habilidades finalizada

---
+++ components/tec-skills.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="tec personal_container">
+    <h2>Tecnologias e habilidades</h2>
+    <img src="../assets/img/tecs.svg" alt="">
+  </section>
+</template>
+
+<style lang="scss">
+.tec { @apply mt-12 md:mt-20 mb-20 mx-auto text-center flex flex-col items-center justify-center; }
+.tec h2 { @apply text-white font-bold md:text-5xl text-2xl; }
+.tec img { @apply mt-12 md:mt-20 max-w-[70%] md:max-w-[35%]; }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,14 @@ import Vue from 'vue'
 import MainHeader from '../components/main-header.vue'
 import CallToAction from '../components/call-to-action.vue'
 import AboutMe from '../components/about-me.vue'
+import TecSkills from '../components/Tec-skills.vue'
 export default Vue.extend({
   name: 'IndexPage',
   components: {
-    MainHeader,
-    CallToAction,
-    AboutMe,
+  MainHeader,
+  CallToAction,
+  AboutMe,
+  TecSkills
   }
 })
 </script>
@@ -17,5 +19,6 @@ export default Vue.extend({
   <MainHeader/>
   <CallToAction/>
   <AboutMe/>
+  <TecSkills/>
   </section>
 </template>
